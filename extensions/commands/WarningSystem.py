a17ad0bb17eae3941ceee38b4bf7f955a68506ee
MajesticCodes <saararonmathias1@gmail.com>
2023-04-15 16:58:17 +0300

    Some tweaks

--- extensions/commands/WarningSystem.py
+++ extensions/commands/WarningSystem.py
@@ -10,7 +10,6 @@ from utils import DataManager
 class WarningSystem(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
 
     @app_commands.command(
         name="warn", description="Warns the mentioned user with a custom warning reason"
@@ -19,6 +18,26 @@ class WarningSystem(commands.Cog):
     async def warn(
         self, interaction: discord.Interaction, user: discord.User, *, reason: str
     ):
+        if user.id == interaction.user.id:
+            return await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Error",
+                    description="You can't warn yourself",
+                    timestamp=datetime.utcnow(),
+                    colour=discord.Colour.orange(),
+                )
+            )
+
+        if interaction.user.top_role.position <= user.top_role.position:
+            return await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Error",
+                    description="You can't warn someone with a higher role than you",
+                    timestamp=datetime.utcnow(),
+                    colour=discord.Colour.orange(),
+                )
+            )
+
         user_id = str(user.id)
         DataManager.register_warning(
             interaction.guild.id,
@@ -70,7 +89,6 @@ class WarningSystem(commands.Cog):
     )
     @app_commands.default_permissions(manage_messages=True)
     async def warnings(self, interaction: discord.Interaction, member: discord.Member):
-
         warnings = DataManager.get_guild_data(interaction.guild.id)["warned_users"]
 
         if len(warnings) == 0:
@@ -82,7 +100,7 @@ class WarningSystem(commands.Cog):
                     colour=discord.Colour.green(),
                 )
             )
-        
+
         e = discord.Embed(
             title="Warnings",
             timestamp=datetime.utcnow(),
@@ -95,10 +113,11 @@ class WarningSystem(commands.Cog):
                     e.add_field(
                         name=f"UUID: `{warn_uuid}`",
                         value=f"\n{warn[warn_uuid]}",
-                        inline=False
+                        inline=False,
                     )
         e.set_author(name=member.name, icon_url=member.avatar.url)
         await interaction.response.send_message(embed=e)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(WarningSystem(bot))
