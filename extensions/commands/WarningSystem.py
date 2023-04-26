5ad316b74c49d949d97f917a5cb4838911db1ca8
MajesticCodes <saararonmathias1@gmail.com>
2023-04-26 10:21:39 +0300

    Fixed warnings command and warning somebody will get logged now

--- extensions/commands/WarningSystem.py
+++ extensions/commands/WarningSystem.py
@@ -19,6 +19,11 @@ class WarningSystem(commands.Cog):
     async def warn(
         self, interaction: discord.Interaction, user: discord.User, *, reason: str
     ):
+        guild_data = DataManager.get_guild_data(interaction.guild.id)
+        logs_channel = logs_channel = self.bot.get_channel(
+            guild_data["logs_channel_id"]
+        )
+
         if user.id == interaction.user.id:
             return await interaction.response.send_message(
                 embed=discord.Embed(
@@ -35,6 +40,15 @@ class WarningSystem(commands.Cog):
                 )
             )
 
+        bot = interaction.guild.get_member(self.bot.user.id)
+        if bot.top_role <= user.top_role:
+            return await interaction.response.send_message(
+                embed=discord.Embed(
+                    description="<:white_cross:1096791282023669860> I can't warn my superiors",
+                    colour=discord.Colour.red(),
+                )
+            )
+
         user_id = str(user.id)
         DataManager.register_warning(
             interaction.guild.id,
@@ -42,25 +56,27 @@ class WarningSystem(commands.Cog):
             f"{reason} - Warned by {interaction.user.name}#{interaction.user.discriminator}",
         )
 
+        if logs_channel != None:
+            embed = discord.Embed(
+                description=f"{user.mention} has been warned for: ```{reason}```",
+                colour=discord.Colour.red(),
+            )
+            embed.add_field(
+                name="Warned by", value=f"{interaction.user.mention}", inline=False
+            )
+            if user.avatar == None:
+                embed.set_author(name=user.name, icon_url=user.default.display_avatar)
+            else:
+                embed.set_author(name=user.name, icon_url=user.avatar)
+            embed.timestamp = datetime.utcnow()
+
+            await logs_channel.send(embed=embed)
         return await interaction.response.send_message(
             embed=discord.Embed(
                 description=f"<:white_checkmark:1096793014287995061> {user.name} has been warned for: ```{reason}```",
                 colour=discord.Colour.green(),
             )
         )
-
-    @warn.error
-    async def on_warn_error(
-        self, interaction: discord.Interaction, error: app_commands.AppCommandError
-    ):
-        if isinstance(error, app_commands.CommandOnCooldown):
-            await interaction.response.send_message(
-                ephemeral=True,
-                embed=discord.Embed(
-                    description=f"<:white_cross:1096791282023669860> Wait {error.retry_after:.1f} seconds before using this command again.",
-                    colour=discord.Colour.red(),
-                ),
-            )
 
     @app_commands.command(name="delwarn", description="Deletes the warning by UUID")
     @app_commands.default_permissions(manage_messages=True)
@@ -89,28 +105,17 @@ class WarningSystem(commands.Cog):
             )
         )
 
-    @delwarn.error
-    async def on_delwarn_error(
-        self, interaction: discord.Interaction, error: app_commands.AppCommandError
-    ):
-        if isinstance(error, app_commands.CommandOnCooldown):
-            await interaction.response.send_message(
-                ephemeral=True,
-                embed=discord.Embed(
-                    description=f"<:white_cross:1096791282023669860> Wait {error.retry_after:.1f} seconds before using this command again.",
-                    colour=discord.Colour.red(),
-                ),
-            )
-
     @app_commands.command(
         name="warnings", description="get the warning list of the user"
     )
     @app_commands.default_permissions(manage_messages=True)
     @app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild.id, i.user.id))
     async def warnings(self, interaction: discord.Interaction, member: discord.Member):
-        warnings = DataManager.get_guild_data(interaction.guild.id)["warned_users"]
+        warnings = DataManager.get_guild_data(interaction.guild.id)["warned_users"].get(
+            str(member.id), None
+        )
 
-        if len(warnings) == 0:
+        if warnings is None or len(warnings) == 0:
             return await interaction.response.send_message(
                 embed=discord.Embed(
                     description=f"<:white_checkmark:1096793014287995061> {member.mention} has no warnings",
@@ -123,30 +128,18 @@ class WarningSystem(commands.Cog):
             timestamp=datetime.utcnow(),
             colour=discord.Colour.orange(),
         )
-
-        for user in warnings:
-            for warn in warnings[user]:
-                for warn_uuid in warn:
-                    e.add_field(
-                        name=f"UUID: `{warn_uuid}`",
-                        value=f"\n{warn[warn_uuid]}",
-                        inline=False,
-                    )
-        e.set_author(name=member.name, icon_url=member.avatar.url)
+        for warn in warnings:
+            for warn_uuid in warn:
+                e.add_field(
+                    name=f"UUID: `{warn_uuid}`",
+                    value=f"\n{warn[warn_uuid]}",
+                    inline=False,
+                )
+        if member.avatar is None:
+            e.set_author(name=member.name, icon_url=member.default.display_avatar.url)
+        else:
+            e.set_author(name=member.name, icon_url=member.avatar.url)
         await interaction.response.send_message(embed=e)
-
-    @warnings.error
-    async def on_warnings_error(
-        self, interaction: discord.Interaction, error: app_commands.AppCommandError
-    ):
-        if isinstance(error, app_commands.CommandOnCooldown):
-            await interaction.response.send_message(
-                ephemeral=True,
-                embed=discord.Embed(
-                    description=f"<:white_cross:1096791282023669860> Wait {error.retry_after:.1f} seconds before using this command again.",
-                    colour=discord.Colour.red(),
-                ),
-            )
 
 
 async def setup(bot: commands.Bot):
