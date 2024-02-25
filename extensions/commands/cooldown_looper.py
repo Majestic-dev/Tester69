4aef80a6c5e7a79caef37e8756f9d198ab0083bb
Aron-Mathias <saararonmathias1@gmail.com>
2024-02-25 15:35:35 +0200

    Add a cooldown_looper that removes cooldowns that have ended

---
+++ extensions/commands/cooldown_looper.py
@@ -0,0 +1,30 @@
+import json
+
+import discord
+from discord.ext import commands, tasks
+
+from utils import DataManager
+
+class cooldown_looper(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.add_listener(self.on_ready)
+
+    async def on_ready(self):
+        self.cooldown_loop.start()
+        
+
+    @tasks.loop(seconds=5)
+    async def cooldown_loop(self):
+        all_users = await DataManager.get_all_users()
+        for user in all_users:
+            user_data = await DataManager.get_user_data(user)
+            if user_data["cooldowns"] is not None:
+                cooldowns = json.loads(user_data["cooldowns"])
+                for command in cooldowns:
+                    if cooldowns[command] < discord.utils.utcnow().isoformat():
+                        await DataManager.remove_cooldown(user, command)
+                        print(f"Removed cooldown for {self.bot.fetch_user(user).name} for {command}")
+
+async def setup(bot):
+    await bot.add_cog(cooldown_looper(bot))
